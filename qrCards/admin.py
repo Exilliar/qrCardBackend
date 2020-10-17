8edaf056c57b76555b1432f34aacbef17c045926
exilliar <rorywebber1000@gmail.com>
2020-10-17 01:30:34 +0100

    #2 made admin account and gave it access to db tables

--- qrCards/admin.py
+++ qrCards/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Account, Card, Stat, AccountCards, CardStats
+
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Card)
+admin.site.register(Stat)
+admin.site.register(AccountCards)
+admin.site.register(CardStats)
