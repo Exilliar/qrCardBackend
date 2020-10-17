1f66464e981b16832e41bfdfd3d39487576a705f
exilliar <rorywebber1000@gmail.com>
2020-10-17 01:26:16 +0100

    #2 made tables and applied migrations on local machine

--- qrCardsBackend/settings.py
+++ qrCardsBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qrCards.apps.QrcardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
