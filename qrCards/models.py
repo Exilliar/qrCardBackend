956b7438835af563e28e7ac069d55d4d5dc5714f
exilliar <rorywebber1000@gmail.com>
2020-10-19 22:56:01 +0100

    #6 updated db. Quite a large rewrite, look at changes to model file to see difference

--- qrCards/models.py
+++ qrCards/models.py
@@ -1,34 +1,31 @@
 from django.db import models
 
 
-class Account(models.Model):
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.email
-
-
-class Card(models.Model):
-    title = models.CharField(max_length=200)
-    imgurl = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
-
-
 class Stat(models.Model):
     name = models.CharField(max_length=200)
-    value = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
-class AccountCards(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    card = models.ForeignKey(Card, on_delete=models.CASCADE)
-
-
 class CardStats(models.Model):
-    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    value = models.CharField(max_length=50)
     stat = models.ForeignKey(Stat, on_delete=models.CASCADE)
+
+
+class Card(models.Model):
+    title = models.CharField(max_length=200)
+    imgurl = models.CharField(max_length=200)
+    cardStats = models.ManyToManyField(CardStats)
+
+    def __str__(self):
+        return self.title
+
+
+class Account(models.Model):
+    email = models.EmailField()
+    cards = models.ManyToManyField(Card)
+
+    def __str__(self):
+        return self.email
